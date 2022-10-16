e2e4d0b933fe61759f42abc7d24ef19515172b21
abouabra <aymanbouabra@gmail.com>
2022-10-16 17:22:14 +0100

    1337

---
+++ get_next_line_bonus.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouabra <abouabra@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 15:37:28 by abouabra          #+#    #+#             */
+/*   Updated: 2022/10/16 17:01:05 by abouabra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*read_fd(int fd, char *line)
+{
+	char	str[BUFFER_SIZE + 1];
+	int		counter;
+
+	counter = 1;
+	while (!ft_strchr(line, '\n') && counter != 0)
+	{
+		counter = read(fd, str, BUFFER_SIZE);
+		if (counter == -1)
+			return (0);
+		str[counter] = '\0';
+		line = ft_strjoin(line, str);
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line[INT_MAX];
+	char		*final;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	final = 0;
+	line[fd] = read_fd(fd, line[fd]);
+	if (!line[fd])
+		return (0);
+	final = set_the_line(line[fd]);
+	line[fd] = set_next_line(line[fd]);
+	return (final);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouabra <abouabra@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 07:45:01 by abouabra          #+#    #+#             */
+/*   Updated: 2022/10/16 17:17:06 by abouabra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = malloc(1 * sizeof(char));
+		s1[0] = 0;
+	}
+	if (s2 == NULL)
+		return (0);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (0);
+	i = -1;
+	j = 0;
+	while (s1[++i])
+		str[i] = s1[i];
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = 0;
+	free(s1);
+	return (str);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	if (!s)
+		return (0);
+	while (*(unsigned char *)s && *(unsigned char *)s != (unsigned char)c)
+		s++;
+	if (*(unsigned char *)s == (unsigned char)c)
+		return ((char *)s);
+	return (0);
+}
+
+char	*set_the_line(char *line)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!line[0])
+		return (0);
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (line[i] == '\n')
+		i++;
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (line[i] && line[i] != '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+		str[i++] = '\n';
+	str[i] = 0;
+	return (str);
+}
+
+char	*set_next_line(char *line)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (line[i] == '\0')
+	{
+		free(line);
+		return (0);
+	}
+	str = malloc(sizeof(char) * (ft_strlen(line) - i + 1));
+	if (!str)
+		return (0);
+	i++;
+	while (line[i])
+		str[j++] = line[i++];
+	str[j] = 0;
+	free(line);
+	return (str);
+}
